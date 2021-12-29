319f984be7b9d52a24f7888205d36d59a64f1c0d
Tracy <1002173040@qq.com>
2021-12-28 22:54:06 -0800

    “基本结构”

---
+++ models/Content.js
@@ -0,0 +1,11 @@
+
+/*
+ *   内容相关数据模型
+ *
+ * */
+
+var mongoose = require('mongoose');
+var contentSchema = require('../schemas/contents');
+
+
+module.exports = mongoose.model('Content', contentSchema);
---
+++ models/User.js
@@ -0,0 +1,10 @@
+
+var mongoose = require('mongoose');
+var userSchema = require('../schemas/users');
+
+/*
+*   User
+*
+**/
+
+module.exports = mongoose.model('User',userSchema);
